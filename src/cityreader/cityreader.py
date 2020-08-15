35f959fbbc6cdb789c1817818e59fa326deacf46
adamcpenman <adamcpenman@gmail.com>
2020-08-15 12:39:59 -0500

    stretch?(ish)

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -85,5 +85,8 @@ def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
     # TODO Ensure that the lat and lon valuse are all floats
     # Go through each city and check to see if it falls within
     # the specified coordinates.
+    for city in cities:
+        if (min(lat1, lat2) <= city.lat <= max(lat1, lat2)) and (min(lon1, lon2) <= city.lon <= max(lon1, lon2)):
+            within.append(city)
 
     return within
